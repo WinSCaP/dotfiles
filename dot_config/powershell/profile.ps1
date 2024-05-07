56af55404011a1f7e40746e712d7290970a31f26
Evert-Jelle <EvertJelle.Lachi@NationaalArchief.nl>
2024-05-07 14:48:49 +0200

    x

--- dot_config/powershell/profile.ps1
+++ dot_config/powershell/profile.ps1
@@ -1,1 +1,40 @@
+# -*-mode:powershell-*- vim:ft=powershell
+
+# ~/.config/powershell/profile.ps1
+# =============================================================================
+# Executed when PowerShell starts.
+#
+# On Windows, this file will be copied over to these locations after
+# running `chezmoi apply` by the script `../../run_powershell.bat.tmpl`:
+#     - %USERPROFILE%\Documents\PowerShell
+#     - %USERPROFILE%\Documents\WindowsPowerShell
+#
+# See https://docs.microsoft.com/en/powershell/module/microsoft.powershell.core/about/about_profiles
+
+$ColorInfo = "DarkYellow"
+$ColorWarn = "DarkRed"
+
+# Includes
+# -----------------------------------------------------------------------------
+
+# Determine user profile parent directory.
+$ProfilePath=Split-Path -parent $profile
+
+# Load functions declarations from separate configuration file.
+if (Test-Path $ProfilePath/functions.ps1) {
+    . $ProfilePath/functions.ps1
+}
+
+# Load alias definitions from separate configuration file.
+if (Test-Path $ProfilePath/aliases.ps1) {
+    . $ProfilePath/aliases.ps1
+}
+
+# Load custom code from separate configuration file.
+if (Test-Path $ProfilePath/extras.ps1) {
+    . $ProfilePath/extras.ps1
+}
+
+
 Invoke-Expression (&starship init powershell)
+
