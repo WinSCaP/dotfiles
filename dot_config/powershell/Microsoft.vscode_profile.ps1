a4284d14efb6df29958aa363a6677c7e37712629
Evert-Jelle <EvertJelle.Lachi@NationaalArchief.nl>
2024-06-06 08:57:50 +0200

    Update .config/powershell/Microsoft.PowerShell_profile.ps1 Update .config/powershell/Microsoft.vscode_profile.ps1 Remove .config/zsh/.zcompdump

--- dot_config/powershell/Microsoft.vscode_profile.ps1
+++ dot_config/powershell/Microsoft.vscode_profile.ps1
@@ -1,3 +1,6 @@
+# Determine user profile parent directory.
+$ProfilePath = Split-Path -parent $profile
+
 if (Test-Path $ProfilePath/profile.ps1) {
-	    . $ProfilePath/profile.ps1
+	. $ProfilePath/profile.ps1
 }
