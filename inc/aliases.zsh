50d6bb5a59265b5b7e407deab75b0008eb3810f2
WinSCaP <site-github.com@lachi.nl>
2024-06-12 21:17:01 +0200

    Update .chezmoi.toml Update .chezmoitemplates/environment-config.sh Update .chezmoitemplates/path-config.sh Add .config/zsh/completions/.keep Add inc/aliases.zsh Add inc/opts.zsh Change attributes of .config/zsh/.zprofile Change attributes of .config/zsh/.zshrc

---
+++ inc/aliases.zsh
@@ -0,0 +1,20 @@
+alias dirs="dirs -v"
+
+alias top="htop"
+alias ip="ip -color=auto"
+
+alias ls="exa --icons -F -H --group-directories-first"
+alias la="exa -lah"
+alias ll="exa -lh"
+alias lag="exa -lah --grid"
+alias llg="exa -lh --grid"
+
+alias grep="rg"
+
+alias cat="bat"
+
+alias nano="nvim"
+alias emacs="nvim"
+alias vim="nvim"
+
+alias kubectl="k"
